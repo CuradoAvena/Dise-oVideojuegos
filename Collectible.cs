182fba422ee46533550e95bd06ee2f8cada3db9a
CuradoAvena <96812008+CuradoAvena@users.noreply.github.com>
2024-03-20 13:35:07 -0600

    Add files via upload

---
+++ Collectible.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Collectibles
+{
+    public class Collectible : MonoBehaviour
+    {
+        public CollectibleType collectibleType;
+
+        // Acceder a la información del coleccionable
+
+        // Método para establecer la información del coleccionable
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                CollectibleManager.Instance.Collect(this);
+
+            }
+        }
+
+        //private void OnTriggerEnter (Collectible collectible)
+        //{
+        //    // Acceder al CollectibleSingleton e invocar el método Open
+        //    CollectibleSingleton.Instance.Open(CollectibleData);
+        //}
+    }
+}
